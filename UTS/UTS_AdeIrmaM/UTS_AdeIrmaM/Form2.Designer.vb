fd437082dff0e4989eeb8e97499f181a721d1b32
Ade <adeirma.m712@gmail.com>
2018-10-19 19:07:46 +0700

    hi
    ya

---
+++ UTS/UTS_AdeIrmaM/UTS_AdeIrmaM/Form2.Designer.vb
@@ -0,0 +1,343 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TBname = New System.Windows.Forms.TextBox()
+        Me.TBidnum = New System.Windows.Forms.TextBox()
+        Me.TBunit = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.TBinst = New System.Windows.Forms.TextBox()
+        Me.TBto = New System.Windows.Forms.TextBox()
+        Me.TBac = New System.Windows.Forms.TextBox()
+        Me.CBtrans = New System.Windows.Forms.ComboBox()
+        Me.TBephp = New System.Windows.Forms.TextBox()
+        Me.BTsend = New System.Windows.Forms.Button()
+        Me.LV1 = New System.Windows.Forms.ListView()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.tbid = New System.Windows.Forms.TextBox()
+        Me.BTedit = New System.Windows.Forms.Button()
+        Me.BTdelet = New System.Windows.Forms.Button()
+        Me.BTupdate = New System.Windows.Forms.Button()
+        Me.TBother = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name of Requestor"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(379, 158)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ext, Phone/Hp"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(379, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Transportation"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(379, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "to Unit/Destination"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 197)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Instructions"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 158)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Date of Issue"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 122)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(26, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Unit"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 84)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(58, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "ID Number"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(432, 291)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 13)
+        Me.Label9.TabIndex = 8
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(379, 84)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(49, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "A/C Reg"
+        '
+        'TBname
+        '
+        Me.TBname.Location = New System.Drawing.Point(139, 45)
+        Me.TBname.Name = "TBname"
+        Me.TBname.Size = New System.Drawing.Size(100, 20)
+        Me.TBname.TabIndex = 10
+        '
+        'TBidnum
+        '
+        Me.TBidnum.Location = New System.Drawing.Point(139, 81)
+        Me.TBidnum.Name = "TBidnum"
+        Me.TBidnum.Size = New System.Drawing.Size(100, 20)
+        Me.TBidnum.TabIndex = 11
+        '
+        'TBunit
+        '
+        Me.TBunit.Location = New System.Drawing.Point(139, 119)
+        Me.TBunit.Name = "TBunit"
+        Me.TBunit.Size = New System.Drawing.Size(100, 20)
+        Me.TBunit.TabIndex = 12
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm:ss"
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(139, 158)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 13
+        Me.DateTimePicker1.Value = New Date(2018, 10, 17, 15, 17, 6, 0)
+        '
+        'TBinst
+        '
+        Me.TBinst.Location = New System.Drawing.Point(139, 197)
+        Me.TBinst.Multiline = True
+        Me.TBinst.Name = "TBinst"
+        Me.TBinst.Size = New System.Drawing.Size(479, 91)
+        Me.TBinst.TabIndex = 14
+        '
+        'TBto
+        '
+        Me.TBto.Location = New System.Drawing.Point(497, 42)
+        Me.TBto.Name = "TBto"
+        Me.TBto.Size = New System.Drawing.Size(100, 20)
+        Me.TBto.TabIndex = 15
+        '
+        'TBac
+        '
+        Me.TBac.Location = New System.Drawing.Point(497, 77)
+        Me.TBac.Name = "TBac"
+        Me.TBac.Size = New System.Drawing.Size(100, 20)
+        Me.TBac.TabIndex = 16
+        '
+        'CBtrans
+        '
+        Me.CBtrans.FormattingEnabled = True
+        Me.CBtrans.Location = New System.Drawing.Point(497, 122)
+        Me.CBtrans.Name = "CBtrans"
+        Me.CBtrans.Size = New System.Drawing.Size(121, 21)
+        Me.CBtrans.TabIndex = 17
+        '
+        'TBephp
+        '
+        Me.TBephp.Location = New System.Drawing.Point(497, 161)
+        Me.TBephp.Name = "TBephp"
+        Me.TBephp.Size = New System.Drawing.Size(100, 20)
+        Me.TBephp.TabIndex = 18
+        '
+        'BTsend
+        '
+        Me.BTsend.Location = New System.Drawing.Point(139, 294)
+        Me.BTsend.Name = "BTsend"
+        Me.BTsend.Size = New System.Drawing.Size(115, 23)
+        Me.BTsend.TabIndex = 19
+        Me.BTsend.Text = "Send Request"
+        Me.BTsend.UseVisualStyleBackColor = True
+        '
+        'LV1
+        '
+        Me.LV1.Location = New System.Drawing.Point(139, 323)
+        Me.LV1.Name = "LV1"
+        Me.LV1.Size = New System.Drawing.Size(612, 134)
+        Me.LV1.TabIndex = 20
+        Me.LV1.UseCompatibleStateImageBehavior = False
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(240, 9)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(118, 13)
+        Me.Label11.TabIndex = 22
+        Me.Label11.Text = "Transportation Request"
+        '
+        'tbid
+        '
+        Me.tbid.Location = New System.Drawing.Point(15, 234)
+        Me.tbid.Name = "tbid"
+        Me.tbid.Size = New System.Drawing.Size(23, 20)
+        Me.tbid.TabIndex = 23
+        '
+        'BTedit
+        '
+        Me.BTedit.Location = New System.Drawing.Point(410, 294)
+        Me.BTedit.Name = "BTedit"
+        Me.BTedit.Size = New System.Drawing.Size(115, 23)
+        Me.BTedit.TabIndex = 24
+        Me.BTedit.Text = "Edit"
+        Me.BTedit.UseVisualStyleBackColor = True
+        '
+        'BTdelet
+        '
+        Me.BTdelet.Location = New System.Drawing.Point(548, 294)
+        Me.BTdelet.Name = "BTdelet"
+        Me.BTdelet.Size = New System.Drawing.Size(115, 23)
+        Me.BTdelet.TabIndex = 25
+        Me.BTdelet.Text = "Deleted"
+        Me.BTdelet.UseVisualStyleBackColor = True
+        '
+        'BTupdate
+        '
+        Me.BTupdate.Location = New System.Drawing.Point(270, 294)
+        Me.BTupdate.Name = "BTupdate"
+        Me.BTupdate.Size = New System.Drawing.Size(115, 23)
+        Me.BTupdate.TabIndex = 26
+        Me.BTupdate.Text = "Update"
+        Me.BTupdate.UseVisualStyleBackColor = True
+        '
+        'TBother
+        '
+        Me.TBother.Location = New System.Drawing.Point(634, 122)
+        Me.TBother.Name = "TBother"
+        Me.TBother.Size = New System.Drawing.Size(132, 20)
+        Me.TBother.TabIndex = 27
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(790, 469)
+        Me.Controls.Add(Me.TBother)
+        Me.Controls.Add(Me.BTupdate)
+        Me.Controls.Add(Me.BTdelet)
+        Me.Controls.Add(Me.BTedit)
+        Me.Controls.Add(Me.tbid)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.LV1)
+        Me.Controls.Add(Me.BTsend)
+        Me.Controls.Add(Me.TBephp)
+        Me.Controls.Add(Me.CBtrans)
+        Me.Controls.Add(Me.TBac)
+        Me.Controls.Add(Me.TBto)
+        Me.Controls.Add(Me.TBinst)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.TBunit)
+        Me.Controls.Add(Me.TBidnum)
+        Me.Controls.Add(Me.TBname)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents TBname As System.Windows.Forms.TextBox
+    Friend WithEvents TBidnum As System.Windows.Forms.TextBox
+    Friend WithEvents TBunit As System.Windows.Forms.TextBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents TBinst As System.Windows.Forms.TextBox
+    Friend WithEvents TBto As System.Windows.Forms.TextBox
+    Friend WithEvents TBac As System.Windows.Forms.TextBox
+    Friend WithEvents CBtrans As System.Windows.Forms.ComboBox
+    Friend WithEvents TBephp As System.Windows.Forms.TextBox
+    Friend WithEvents BTsend As System.Windows.Forms.Button
+    Friend WithEvents LV1 As System.Windows.Forms.ListView
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents tbid As System.Windows.Forms.TextBox
+    Friend WithEvents BTedit As System.Windows.Forms.Button
+    Friend WithEvents BTdelet As System.Windows.Forms.Button
+    Friend WithEvents BTupdate As System.Windows.Forms.Button
+    Friend WithEvents TBother As System.Windows.Forms.TextBox
+End Class
